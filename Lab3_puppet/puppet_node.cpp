443e70724818e0ae6b5890584b888a2da34d362f
soso0024 <chuang.0024@gmail.com>
2024-04-15 09:54:08 +0200

    add lab3 prog

--- Lab3_puppet/puppet_node.cpp
+++ Lab3_puppet/puppet_node.cpp
@@ -26,10 +26,13 @@ public:
     // init whatever is needed for your node
 
     // init command message for left arm
+    left_arm_command_ = JointCommand();
 
     // init publisher to left arm command
+    command_publisher_ = this->create_publisher<JointCommand>("left_arm_command", 10);
 
     // init timer - the function publishCommand() should called with the given rate
+    timer_ = this->create_wall_timer(1000ms, std::bind(&PuppetNode::publishCommand, this));
 
     // IK service wrapper into IKNode
     ik_node.init("ik_node","/ExternalTools/left/PositionKinematicsNode/IKService");
@@ -51,25 +54,30 @@ private:
     if(tf_buffer.canTransform("left_gripper_desired", "base", tf2::TimePointZero, tf2::durationFromSec(1.0)))
     {
       // get this transform with tf_buffer.lookupTransform("base", "left_gripper_desired", ...
-
+	  auto transform = tf_buffer.lookupTransform("base", "left_gripper_desired", tf2::TimePointZero);
 
       // build service request SolvePositionIK::Request from obtained transform
       SolvePositionIK::Request req;
-
-
-
-
-
-
+	  req.pose.header.frame_id = "base";
+	  req.pose.pose.position.x = transform.transform.translation.x;
+	  req.pose.pose.position.y = transform.transform.translation.y;
+	  req.pose.pose.position.z = transform.transform.translation.z;
+	  req.pose.pose.orientation = transform.transform.rotation;
+      
       // call service and get response
-      if(SolvePositionIK::Response res; ik_node.call(req, res))
-      {
-        // call to IK was successfull, check if the solution is valid
-
-        // copy response data to joint command and publish to left arm
-
-
-      }
+      if (ik_node.call(req, res) && res.is_valid)
+	  {
+		  // call to IK was successfull, check if the solution is valid
+		  JointCommand command;
+		  command.mode = JointCommand::POSITION_MODE;
+		  for (auto& joint : res.joints)
+		  {
+			command.names.push_back(joint.name);
+			command.command.push_back(joint.position);
+		  }
+		  // copy response data to joint command and publish to left arm
+		  command_publisher_->publish(command);
+	   }
     }
   }
 };
