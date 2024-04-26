7341530f7e238e6e02f8d86eb9229fd58df96f01
soso0024 <chuang.0024@gmail.com>
2024-04-26 18:29:57 +0200

    finish lab3

---
+++ Lab3_puppet/launch/puppet_launch.py
@@ -0,0 +1,25 @@
+from simple_launch import SimpleLauncher
+
+def generate_launch_description():
+    sl = SimpleLauncher()
+
+    sl.declare_arg('x', '0')
+    sl.declare_arg('y', '0')
+    sl.declare_arg('z', '0.1')
+    sl.declare_arg('yaw', '0')
+    sl.declare_arg('pitch', '3.14')
+    sl.declare_arg('roll', '0')
+    sl.declare_arg('frame_id', 'right_gripper')
+    sl.declare_arg('child_frame_id', 'left_gripper_desired')
+
+    sl.node(package = 'tf2_ros',
+            executable = 'static_transform_publisher',
+            arguments = [sl.arg('x'), sl.arg('y'), sl.arg('z'),
+                        sl.arg('yaw'), sl.arg('pitch'), sl.arg('roll'),
+                        sl.arg('frame_id'), sl.arg('child_frame_id')])
+
+    sl.include(package = 'baxter_simple_sim',
+               launch_file = 'sim_launch.py',
+               launch_arguments = {'lab':'puppet'})
+
+    return sl.launch_description()
--- Lab3_puppet/puppet_node.cpp
+++ Lab3_puppet/puppet_node.cpp
@@ -26,70 +26,218 @@ public:
     // init whatever is needed for your node
 
     // init command message for left arm
-    left_arm_command_ = JointCommand();
 
     // init publisher to left arm command
-    command_publisher_ = this->create_publisher<JointCommand>("left_arm_command", 10);
+    publisher_ = this->create_publisher<JointCommand>("robot/limb/left/joint_command", 10);
 
     // init timer - the function publishCommand() should called with the given rate
-    timer_ = this->create_wall_timer(1000ms, std::bind(&PuppetNode::publishCommand, this));
+    timer_ = this->create_wall_timer(100ms, std::bind(&PuppetNode::publishCommand, this));
 
     // IK service wrapper into IKNode
     ik_node.init("ik_node","/ExternalTools/left/PositionKinematicsNode/IKService");
   }
-  
+
 private:
 
   // declare member variables for command publisher and timer
-  JointCommand left_arm_command_;   // Declare the JointCommand for the left arm
-  rclcpp::Publisher<JointCommand>::SharedPtr command_publisher_;  // Declare the publisher
-  rclcpp::TimerBase::SharedPtr timer_;  // Declare the timer
-
-  // TF 2 and IK service declarations remain the same
+  rclcpp::Publisher<JointCommand>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
   ServiceNodeSync<SolvePositionIK> ik_node;
-  tf2_ros::Buffer tf_buffer{get_clock()};                // stores all previous elementary transforms in a tree
-  tf2_ros::TransformListener tf_listener{tf_buffer};   // subscribes to /tf
 
-   void publishCommand()
-   {
+  // TF 2 stuff
+  tf2_ros::Buffer tf_buffer{get_clock()};                // stores all previous elementary transforms in a tree
+  tf2_ros::TransformListener tf_listener{tf_buffer};     // subscribes to /tf
+
+  void publishCommand()
+  {
+    geometry_msgs::msg::TransformStamped transform;
+    baxter_core_msgs::msg::JointCommand command_msg;
+
+    // check if the transform from base to left_gripper_desired is available
     if(tf_buffer.canTransform("left_gripper_desired", "base", tf2::TimePointZero, tf2::durationFromSec(1.0)))
     {
-        auto transform = tf_buffer.lookupTransform("base", "left_gripper_desired", tf2::TimePointZero);
-        SolvePositionIK::Request req;
-        geometry_msgs::msg::PoseStamped pose_stamped;
-        pose_stamped.header.frame_id = "base";
-        pose_stamped.header.stamp = rclcpp::Clock().now();
-        pose_stamped.pose.position.x = transform.transform.translation.x;
-        pose_stamped.pose.position.y = transform.transform.translation.y;
-        pose_stamped.pose.position.z = transform.transform.translation.z;
-        pose_stamped.pose.orientation.w = transform.transform.rotation.w;
-        pose_stamped.pose.orientation.x = transform.transform.rotation.x;
-        pose_stamped.pose.orientation.y = transform.transform.rotation.y;
-        pose_stamped.pose.orientation.z = transform.transform.rotation.z;
-        req.pose_stamp.push_back(pose_stamped);
+      // get this transform with tf_buffer.lookupTransform("base", "left_gripper_desired", ...
+      transform = tf_buffer.lookupTransform("base", "left_gripper_desired", tf2::TimePointZero);
 
-        if (SolvePositionIK::Response res; ik_node.call(req, res))
-        {
-            JointCommand command;
-            command.mode = JointCommand::POSITION_MODE;
-            for (const auto& joint : res.joints)
-            {
-                for (const auto& name : joint.name)
-                {
-                    command.names.push_back(name);
-                }
-                for (const auto& position : joint.position)
-                {
-                    command.command.push_back(position);
-                }
-            }
-            command_publisher_->publish(command);
-        }
+      // build service request SolvePositionIK::Request from obtained transform
+      SolvePositionIK::Request req;
+      geometry_msgs::msg::PoseStamped pose_stamped;
+
+      pose_stamped.pose.position.x = transform.transform.translation.x;
+      pose_stamped.pose.position.y = transform.transform.translation.y;
+      pose_stamped.pose.position.z = transform.transform.translation.z;
+      pose_stamped.pose.orientation.x = transform.transform.rotation.x;
+      pose_stamped.pose.orientation.y = transform.transform.rotation.y;
+      pose_stamped.pose.orientation.z = transform.transform.rotation.z;
+
+      req.pose_stamp.push_back(pose_stamped);
+      command_msg.set__mode(1);
+      command_msg.set__names({"left_s0", "left_s1","left_e0","left_e1","left_w0","left_w1","left_w2"});
+      command_msg.command.resize(command_msg.names.size(),0.0);
+      // call service and get response
+      if(SolvePositionIK::Response res; ik_node.call(req, res))
+      {
+        // call to IK was successfull, check if the solution is valid
+          if(res.is_valid[0]){
+              std::copy(res.joints[0].position.begin(), res.joints[0].position.end(), command_msg.command.begin());
+              publisher_->publish(command_msg);
+          }
+        // copy response data to joint command and publish to left arm
+
+
+      }
     }
-   }
+  }
 };
 }
 
 
 #include "rclcpp_components/register_node_macro.hpp"
 RCLCPP_COMPONENTS_REGISTER_NODE(lab3_puppet::PuppetNode)
+
+
+
+/*
+[ROS2@baxter] ~/ros2$ ros2 interface show sensor_msgs/msg/JointState
+# This is a message that holds data to describe the state of a set of torque controlled joints.
+#
+# The state of each joint (revolute or prismatic) is defined by:
+#  * the position of the joint (rad or m),
+#  * the velocity of the joint (rad/s or m/s) and
+#  * the effort that is applied in the joint (Nm or N).
+#
+# Each joint is uniquely identified by its name
+# The header specifies the time at which the joint states were recorded. All the joint states
+# in one message have to be recorded at the same time.
+#
+# This message consists of a multiple arrays, one for each part of the joint state.
+# The goal is to make each of the fields optional. When e.g. your joints have no
+# effort associated with them, you can leave the effort array empty.
+#
+# All arrays in this message should have the same size, or be empty.
+# This is the only way to uniquely associate the joint name with the correct
+# states.
+
+std_msgs/Header header
+        builtin_interfaces/Time stamp
+                int32 sec
+                uint32 nanosec
+        string frame_id
+
+string[] name
+float64[] position
+float64[] velocity
+float64[] effort
+
+
+
+[ROS2@baxter] ~/ros2$ ros2 interface show baxter_core_msgs/srv/SolvePositionIK
+# Endpoint Pose(s) to request Inverse-Kinematics joint solutions for.
+geometry_msgs/PoseStamped[] pose_stamp
+        std_msgs/Header header
+                builtin_interfaces/Time stamp
+                        int32 sec
+                        uint32 nanosec
+                string frame_id
+        Pose pose
+                Point position
+                        float64 x
+                        float64 y
+                        float64 z
+                Quaternion orientation
+                        float64 x 0
+                        float64 y 0
+                        float64 z 0
+                        float64 w 1
+
+# (optional) Joint Angle Seed(s) for IK solver.
+# * specify a JointState seed for each pose_stamp, using name[] and position[]
+# * empty arrays or a non-default seed_mode will cause user seed to not be used
+sensor_msgs/JointState[] seed_angles
+        #
+        #
+        #
+        #
+        std_msgs/Header header
+                builtin_interfaces/Time stamp
+                        int32 sec
+                        uint32 nanosec
+                string frame_id
+        string[] name
+        float64[] position
+        float64[] velocity
+        float64[] effort
+
+# Seed Type Mode
+# * default (SEED_AUTO) mode: iterate through seed types until first valid
+#                             solution is found
+# * setting any other mode:   try only that seed type
+uint8 SEED_AUTO = 0
+uint8 SEED_USER = 1
+uint8 SEED_CURRENT = 2
+uint8 SEED_NS_MAP = 3
+
+uint8 seed_mode
+---
+# joints[i]      == joint angle solution for each pose_state[i]
+sensor_msgs/JointState[] joints
+        #
+        #
+        #
+        #
+        std_msgs/Header header
+                builtin_interfaces/Time stamp
+                        int32 sec
+                        uint32 nanosec
+                string frame_id
+        string[] name
+        float64[] position
+        float64[] velocity
+        float64[] effort
+
+# NOTE: isValid will be deprecated by result_type in future versions
+bool[] is_valid
+
+# result_type[i] == seed type used to find valid solution, joints[i];
+# otherwise,     == RESULT_INVALID (no valid solution found).
+uint8 RESULT_INVALID = 0
+uint8[] result_type
+
+
+
+[ROS2@baxter] ~/ros2$ ros2 interface show geometry_msgs/msg/TransformStamped
+# This expresses a transform from coordinate frame header.frame_id
+# to the coordinate frame child_frame_id at the time of header.stamp
+#
+# This message is mostly used by the
+# <a href="https://index.ros.org/p/tf2/">tf2</a> package.
+# See its documentation for more information.
+#
+# The child_frame_id is necessary in addition to the frame_id
+# in the Header to communicate the full reference for the transform
+# in a self contained message.
+
+# The frame id in the header is used as the reference frame of this transform.
+std_msgs/Header header
+        builtin_interfaces/Time stamp
+                int32 sec
+                uint32 nanosec
+        string frame_id
+
+# The frame id of the child frame to which this transform points.
+string child_frame_id
+
+# Translation and rotation in 3-dimensions of child_frame_id from header.frame_id.
+Transform transform
+        Vector3 translation
+                float64 x
+                float64 y
+                float64 z
+        Quaternion rotation
+                float64 x 0
+                float64 y 0
+                float64 z 0
+                float64 w 1
+
+*/
+
